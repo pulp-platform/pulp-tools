6a4c1e44a85df80852433599e687335549443344
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-10-16 19:19:48 +0200

    Switch pulp to single regfile for floats

--- bin/plpflags.py
+++ bin/plpflags.py
@@ -790,7 +790,7 @@ class Arch(object):
       c_flags = ' -mPE=8 -mFC=1'
       ld_flags = ' -mPE=8 -mFC=1'
       isa='imcXgap8'
-    elif self.chip in ['vega', 'gap9']:
+    elif self.chip in ['vega', 'gap9', 'pulp']:
       c_flags = ' -mPE=8 -mFC=1'
       ld_flags = ' -mPE=8 -mFC=1'
       isa='imcXgap9'
